649e4f261151b5f506350d34d41b555f86bae73e
Varsha Maroju <Varshamaroju2k4@gmail.com>
2025-07-18 15:05:54 +0530

    Added Addition.java program

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+    public static int add(String args[]){
+        return a+b;
+    }
+    public static void main(String args[]){
+        int sum=add(15,25);
+        System.out.println("Sum of 15 and 25 is: "+sum);
+    }
+}
